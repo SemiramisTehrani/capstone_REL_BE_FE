9bb5bb92096982c51123801ca2cea5fec7f8cb61
unknown <semtehrani@yahoo.com>
2022-09-02 20:37:54 -0700

    GET all contatcs works in postman (unprotected)

--- backend/contactus/urls.py
+++ backend/contactus/urls.py
@@ -1,7 +1,6 @@
-# from django.urls import path
-# from contactus import views
+from django.urls import path, include
+from contactus import views
 
-# urlpatterns = [
-#     path('all/', views.get_all_contactus),
-#     path('all/', views.get_all_contactus_id),
-# ]
+urlpatterns = [
+    path('all/', views.get_all_contactus),
+]
--- backend/contactus/views.py
+++ backend/contactus/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+
+
+from .models import ContactUs
+from .serializers import ContactusSerializer
 
 # Create your views here.
+
+# @api_view(['GET'],['POST'])
+# @permission_classes([AllowAny])
+# def get_all_contactus(request):
+    
+#     if request.method == 'GET' : 
+#         contacts = ContactUs.objects.all()
+#         serializer = ContactUsSerializer(contacts, many=True)
+#         return Response(serializer.data)
+    
+#     elif request.method == 'POST' :
+#          serializer = ContactUsSerializer(data = request.data)
+#          serializer.is_valid(raise_exception = True)
+#          serializer.save()
+#          return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_contactus(request):
+    contacts = ContactUs.objects.all()
+    serializer = ContactusSerializer(contacts, many=True)
+    return Response(serializer.data)
