77ff21a5ff12660c676560c85196bc3a0ceca878
unknown <semtehrani@yahoo.com>
2022-09-03 13:44:32 -0700

    created consultation app, contactus app : initialized user_id (in serializers.py) to remove null error during POST in Postman

--- backend/contactus/serializers.py
+++ backend/contactus/serializers.py
@@ -1,9 +1,11 @@
+from asyncore import write
 from rest_framework import serializers
 from .models import ContactUs
 
 class ContactusSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactUs
-        fields = ['id', 'fullname', 'email', 'subject', 'message','user_id']
-        # fields = ['id', 'fullname', 'email', 'subject', 'message']
+        fields = ['id', 'fullname', 'email', 'subject', 'message', 'user_id']   # user_id & fullname tie toogether under foreignkey
         depth = 1
+        
+    user_id = serializers.IntegerField (write_only = True)
