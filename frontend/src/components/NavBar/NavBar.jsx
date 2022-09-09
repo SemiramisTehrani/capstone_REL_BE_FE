a79083f563db042ad8a3c0540bb10989101a809d
unknown <semtehrani@yahoo.com>
2022-09-09 11:21:54 -0700

    added picture , autoemail in backend, NavBr is updated

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -26,34 +26,56 @@
 //   );
 // };
 
-import React from "react";
-import { Link } from "react-router-dom";
-import SearchBar from "../SearchBar/SearchBar";
-import { Navbar, Button, Container } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import SearchBar from "../SearchBar/SearchBar";
+// import { Navbar, Button, Container } from "react-bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
-const NavBar = (props) => {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">YouTube-by-Semi</Navbar.Brand>
+// const NavBar = (props) => {
+//   return (
+//     <Navbar bg="light" expand="lg">
+//       <Container fluid>
+//         <Navbar.Brand href="#">YouTube-by-Semi</Navbar.Brand>
 
-        <SearchBar
-          search={props.search}
-          setSearch={props.setSearch}
-          getVideo={props.getVideo}
-        />
-        <Link to="loginform">
-          <li>Login</li>
-        </Link>
-        <Button>Logout</Button> 
-      </Container>
-    </Navbar>
+//         <SearchBar
+//           search={props.search}
+//           setSearch={props.setSearch}
+//           getVideo={props.getVideo}
+//         />
+//         <Link to="loginform">
+//           <li>Login</li>
+//         </Link>
+//         <Button>Logout</Button> 
+//       </Container>
+//     </Navbar>
 
   
+//   );
+// };
+
+// export default NavBar;
+
+// Rose Electronics Lab  : header & NavBar
+import React from 'react';
+import { useContext } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import AuthContext from '../../context/AuthContext';
+import './Navbar.css';
+
+
+
+const NavBar = (props) => {
+
+  return (
+    <div>
+      <header> 
+    <title>Rose Electronics Lab</title>
+    </header>
+    <hr></hr>   
+
+    </div>
   );
 };
 
 export default NavBar;
-
-
